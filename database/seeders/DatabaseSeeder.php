88729b2bb9641f81d2d0002b21901a2197ef8204
raphaeldanu <raphaeldanu@gmail.com>
2022-05-28 21:10:26 +0700

    Add User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserLevel;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => "Raphael Adhimas A. S.",
+            'username' => 'admin',
+            'email' => 'testadmin@gmail.com',
+            'level' => UserLevel::Admin,
+            'password' => Hash::make('12345678'),
+        ]);
     }
 }
