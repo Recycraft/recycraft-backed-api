6458dd90e72db6fe6cc11c7817816f0af9ecd542
raphaeldanu <raphaeldanu@gmail.com>
2022-05-29 19:31:30 +0700

    Fix Login and Register Form

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -29,31 +29,59 @@
       <form action="/register" method="post">
         @csrf
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Full name">
+          <input type="text" class="form-control" name="name" id="name" placeholder="Full name" value="{{ old('name') }}">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
             </div>
           </div>
+          @error('name')
+            <div class="error invalid-feedback">
+              {{ $message }}
+            </div>
+          @enderror
         </div>
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="text" class="form-control" name="username" id="username" placeholder="Full name" value="{{ old('username') }}">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+          @error('username')
+            <div class="error invalid-feedback">
+              {{ $message }}
+            </div>
+          @enderror
+        </div>
+        <div class="input-group mb-3">
+          <input type="email" name="email" id="email" class="form-control" placeholder="Email" value="{{ old('email') }}">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
             </div>
           </div>
+          @error('email')
+            <div class="error invalid-feedback">
+              {{ $message }}
+            </div>
+          @enderror
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" name="password" id="password" class="form-control" placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
+          @error('password')
+            <div class="error invalid-feedback">
+              {{ $message }}
+            </div>
+          @enderror
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
+          <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="Retype password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
