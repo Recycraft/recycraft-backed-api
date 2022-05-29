c9f8f647bd241d593756d2427a44a3abf18f5b2d
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2022-05-30 00:41:54 +0700

    Add comment to Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,43 +25,43 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            'name' => "Raphael Adhimas A. S.",
-            'username' => 'admin',
-            'email' => 'testadmin@gmail.com',
-            'level' => UserLevel::Admin,
-            'password' => Hash::make('12345678'),
-        ]);
+        // User::create([
+        //     'name' => "Raphael Adhimas A. S.",
+        //     'username' => 'admin',
+        //     'email' => 'testadmin@gmail.com',
+        //     'level' => UserLevel::Admin,
+        //     'password' => Hash::make('12345678'),
+        // ]);
 
-        ScrapCategory::create([
-            'name' => 'Plastic'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Paper'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Cardboard'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Styrofoam'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Bottle'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Can'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Spoon Plastic'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Stick'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Straw'
-        ]);
-        ScrapCategory::create([
-            'name' => 'Plastic Cup'
-        ]);
+        // ScrapCategory::create([
+        //     'name' => 'Plastic'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Paper'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Cardboard'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Styrofoam'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Bottle'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Can'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Spoon Plastic'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Stick'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Straw'
+        // ]);
+        // ScrapCategory::create([
+        //     'name' => 'Plastic Cup'
+        // ]);
     }
 }
