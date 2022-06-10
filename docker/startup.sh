6ea76f52774d711134c756880f71af79960bfc00
raphaeldanu <raphaeldanu@gmail.com>
2022-06-10 16:12:27 +0700

    Fix docker setup

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,6 +4,6 @@ sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
 php-fpm -D
 
-while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
+#while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
 nginx
