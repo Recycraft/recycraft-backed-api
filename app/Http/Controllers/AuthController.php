0b8d4fe8559073ceceda718157e904930230e007
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2022-06-11 13:26:25 +0700

    #Issue1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,7 +41,6 @@ class AuthController extends Controller
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
-            'device_name' => 'required',
         ]);
 
         $user = User::where('email', $request->email)->first();
@@ -99,10 +98,10 @@ class AuthController extends Controller
             $request->session()->regenerate();
 
             if ($request->user()->level == UserLevel::User){
-                return redirect()->route('user.dashboard');
+                return redirect()->secure_url('/user/dashboard');
             }
 
-            return redirect()->route('dashboard');
+            return redirect()->secure_url('/admin/dashboard');
         }
 
         return back()->withErrors([
@@ -128,7 +127,7 @@ class AuthController extends Controller
         ];
 
         if (User::create($data)) {
-            return redirect()->route('login');
+            return redirect()->secure_url('login');
         } else {
             return back()->withErrors([
                 'msg' => 'Gagal Register'
